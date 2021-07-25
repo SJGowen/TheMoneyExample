9e34b82920e8b288cd71384d3981d76d83c4b56f
Stephen Gowen <sjgowen@gmail.com>
2021-07-25 15:16:59 +0100

    Added failing TestEquality().

--- WyCashTests/MoneyTests.cs
+++ WyCashTests/MoneyTests.cs
@@ -7,13 +7,19 @@ namespace WyCashTests
     public class MoneyTests
     {
         [Fact]
-        public void testMultiplication()
+        public void TestMultiplication()
         {
             Dollar five = new Dollar(5);
             Dollar fiveXtwo = five.Times(2);
             Assert.Equal(10, fiveXtwo.Amount);
             Dollar fiveXthree = five.Times(3);
             Assert.Equal(15, fiveXthree.Amount);
+        }
+
+        [Fact]
+        public void TestEquality()
+        {
+            Assert.True(new Dollar(5).Equals(new Dollar(5)));
         }
     }
 }
