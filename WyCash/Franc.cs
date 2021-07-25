51ef77578fd576d2fd8d836f5b5397000143a207
Stephen Gowen <sjgowen@gmail.com>
2021-07-25 17:16:36 +0100

    Created a Franc object that is a copy of the Dollar object. Wrote TestFrancMultiplication, which is a copy of TestMultiplication.

---
+++ WyCash/Franc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WyCash
+{
+    public record Franc
+    {
+        private int Amount;
+
+        public Franc(int amount)
+        {
+            Amount = amount;
+        }
+
+        public Franc Times(int multiplier) => new(Amount * multiplier);
+    }
+}
